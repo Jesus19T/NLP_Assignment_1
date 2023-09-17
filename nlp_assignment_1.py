8ba0eda704627f2dfdd5ca01d7fb964131904d9d
jn2112 <93005114+jn2112@users.noreply.github.com>
2023-09-17 17:46:49 -0500

    preprocess

--- nlp_assignment_1.py
+++ nlp_assignment_1.py
@@ -1,1 +1,27 @@
-print("Hello World")
+import os
+
+
+def preprocess (filePath):
+    fileWords = []
+    with open(filePath, 'r', errors = 'ignore') as f:
+       lines = f.read().split(' ') #parse words 
+    
+    
+    fileWords=[x.lower() for x in lines if x.isalpha()] #remove punctuation
+                            
+            
+    return fileWords
+
+def main():
+    #read files
+    path = os.getcwd()
+    newFile = path+"\\train.txt"
+    
+    trainingData = preprocess(newFile)
+    print(trainingData[0:30])
+    
+    
+    
+
+if __name__ == "__main__":
+    main()print("Hello World")
