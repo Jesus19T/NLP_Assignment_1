b0211b99da10f93c49081c301aa65b90f8791f56
jn2112 <93005114+jn2112@users.noreply.github.com>
2023-09-27 17:15:14 -0500

    Add files via upload

--- nlp_assignment_1.py
+++ nlp_assignment_1.py
@@ -1,31 +1,69 @@
 import os
+import string
+import re
 
 
 def preprocess (filePath):
     fileWords = []
+    parseWords = []
+    parsePhrase = []
+   # punc ="[\[\]# .,!/\"'-:;|?*“”’‘_]"
     with open(filePath, 'r', errors = 'ignore') as f:
-       lines = f.read().split(' ') #parse words 
+       lines = f.readlines() #parse words
     
-    
-    fileWords=[x.lower() for x in lines if x.isalpha()] #remove punctuation
-    #fileWords=[x.lower() for x in lines if x not in string.punctuation] #remove punctuation                        
-            
-    return fileWords
+ 
+    for x in lines:    
+        y = x.split('\n')       #remove return
+        lineLower = y[0].lower()        #lowercase
+        fileWords.append(("<start>") +" " + lineLower + " " + ("<stop>")) #add start and stop tokens
+       
+    for x in fileWords:   
+          
+        temp = re.sub("[()!\[\]#.,/\"'-:;|?*“”’‘_]","",x)
+        temp2 = re.sub("\s+"," ",temp)
+        splitVal = temp2.split(' ') 
+       
+
+        for y in range (0, (len(splitVal))):  
+            parseWords.append(splitVal[y]) #combine to large corpus for each word
+            if(y != 0): 
+                parsePhrase.append(splitVal[y-1]+" "+ splitVal[y]) #create bigram phrases
+            if(y==188): 
+                print("x")
+                    
+    return parseWords, parsePhrase
+
+def unigram(data): 
+    dictionary = {}
+    for words in data: 
+        if(dictionary.get(words)): 
+             num = dictionary.get(words)
+             dictionary.update({words: num + 1}) 
+        else:
+             dictionary.update({words: 1})
+        
+    return dictionary
 
 def main():
     #read files
+    unigramDict = {}
     path = os.getcwd()
-    # newFile = path+"\\A1_DATASET\\train.txt"    #Uncomment for WINDOWS
+    
     newFile = path+"/A1_DATASET/train.txt"    #Uncommment for MAC
     
-    trainingData = preprocess(newFile)
+    unigramSet, bigramSet = preprocess(newFile)
+    
 
     #test 
-    print(trainingData[0:30])
+    print(unigramSet[0:30])
+    print(bigramSet[0:30])
+    
+    unigramDict = unigram(unigramSet)
+    print("Dictionary")
     
     
     
 
 if __name__ == "__main__":
     main()
-    print("Hello World")
+    print("Complete")
